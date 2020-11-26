acbc24f033041313801804c091b8109e669f4f20
Guzooo <guzooo-guzooo@o2.pl>
2020-11-27 00:09:59 +0100

    0.1.0.1
    Wstępnie ułożono wygląd strony

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Organizer Ucznia</title>
+    </head>
+    <body>
+        <div id="baner">
+
+        </div>
+        <div id="lewy">
+
+        </div>
+        <div id="prawy">
+
+        </div>
+    </body>
+    <style>
+        #baner{
+            width:100%;
+            height:10%;
+            background-color:red;
+        }
+        #lewy{
+            width:35%;
+            height:90%;
+            background-color:yellow;
+            float: left;
+        }
+        #prawy{
+            width:65%;
+            height:90%;
+            background-color:green;
+            float: right;
+        }
+    </style>
+</html>

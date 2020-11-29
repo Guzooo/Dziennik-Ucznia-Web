4d859cffbc9f0be2b47de188813b57656966e9e5
Guzooo <guzooo-guzooo@o2.pl>
2020-11-29 11:33:38 +0100

    0.0.2
    Dodano główną cześć strony

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     <head>
         <title>Organizer Ucznia</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="resources/style/style.css">
         <link rel="stylesheet" href="resources/color/color.css">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -9,9 +10,9 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </head>
     <body>
-        <nav class="navbar bgPrimaryTranslucent shadow">
-            <div class="container-fluid">
-                <div class="col-4">
+        <nav class="navbar bgPrimaryTranslucent shadow-sm fixed-top">
+            <div class="container-fluid p-0">
+                <div class="col-5 pl-0">
                     <a class="navbar-brand" href="#">
                         <div class="d-inline-block align-top">
                             <img class="rounded-circle" src="https://lh3.googleusercontent.com/nSanxwfGDbQUXogLXkUq6n5JplTObqs72T1Ym5zvAVhmu6Jr5LodJyrrePtzRT2V7Jw" width="50" height="50" alt="Logo Guzooo">
@@ -23,21 +24,21 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-8">
+                <div class="col-7 pr-0">
                     <div class="row">
                         <span class="col-12 textNormal">Semestr: 1 • Średnia końcowa: 4,00 • Polski za 5 minut</span>
-                        <span class="col-6 textDescription">Średnia końcowa 4.13 • 4</span>
-                        <!-- <span class="col-6 textDescription">Średnia końcowa 4.62 • 5</span> -->
+                        <span class="col-12 textDescription">Średnia końcowa 4.13 • 4</span>
                     </div>
                 </div>
             </div>
         </nav>
-        <div class="container-fluid position-fixed h-100">
-            <div class="row h-100">
-                <div class="col-4 bgAccent h-100"></div>
-                <div class="col-8 bgSecondary h-100"></div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-5">
+                    <?php include 'pages/main/main.php' ?>
+                </div>
+                <div class="col-7"> </div>
             </div>
         </div>
     </body>
-    <style>
 </html>
---
+++ pages/main/main.php
@@ -0,0 +1,161 @@
+<div class="row cardRail vh-100">
+    <div id="top-padding" class="col-12" style='height:80px;'></div>
+    <div class="col-12 card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5 textNormal font-weight-bold m-auto">
+                    Polski
+                </div>
+                <div class="col-2 textNormal text-center m-auto">
+                    NP: 2
+                </div>
+                <div class="col-5 textNormal text-right m-auto">
+                    Ilość notatek: 2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5 textNormal font-weight-bold m-auto">
+                    Polski
+                </div>
+                <div class="col-2 textNormal text-center m-auto">
+                    NP: 2
+                </div>
+                <div class="col-5 textNormal text-right m-auto">
+                    Ilość notatek: 2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5 textNormal font-weight-bold m-auto">
+                    Polski
+                </div>
+                <div class="col-2 textNormal text-center m-auto">
+                    NP: 2
+                </div>
+                <div class="col-5 textNormal text-right m-auto">
+                    Ilość notatek: 2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5 textNormal font-weight-bold m-auto">
+                    Polski
+                </div>
+                <div class="col-2 textNormal text-center m-auto">
+                    NP: 2
+                </div>
+                <div class="col-5 textNormal text-right m-auto">
+                    Ilość notatek: 2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5 textNormal font-weight-bold m-auto">
+                    Polski
+                </div>
+                <div class="col-2 textNormal text-center m-auto">
+                    NP: 2
+                </div>
+                <div class="col-5 textNormal text-right m-auto">
+                    Ilość notatek: 2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5 textNormal font-weight-bold m-auto">
+                    Polski
+                </div>
+                <div class="col-2 textNormal text-center m-auto">
+                    NP: 2
+                </div>
+                <div class="col-5 textNormal text-right m-auto">
+                    Ilość notatek: 2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5 textNormal font-weight-bold m-auto">
+                    Polski
+                </div>
+                <div class="col-2 textNormal text-center m-auto">
+                    NP: 2
+                </div>
+                <div class="col-5 textNormal text-right m-auto">
+                    Ilość notatek: 2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5 textNormal font-weight-bold m-auto">
+                    Polski
+                </div>
+                <div class="col-2 textNormal text-center m-auto">
+                    NP: 2
+                </div>
+                <div class="col-5 textNormal text-right m-auto">
+                    Ilość notatek: 2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5 textNormal font-weight-bold m-auto">
+                    Polski
+                </div>
+                <div class="col-2 textNormal text-center m-auto">
+                    NP: 2
+                </div>
+                <div class="col-5 textNormal text-right m-auto">
+                    Ilość notatek: 2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="bottom-padding" class="col-12 h-25"></div>
+</div>
+<nav class="col-12 bgPrimaryTranslucent position-absolute topShadow" style=" bottom:0; left:0;">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-3 navigation_element">
+                <span class="material-icons">settings_applications</span>
+                <p>Ustawienia</p>
+            </div>
+            <div class="col-3 navigation_element">
+                <span class="material-icons">home</span>
+                <p>Główna</p>
+            </div>
+            <div class="col-3 navigation_element">
+                <span class="material-icons">equalizer</span>
+                <p>Statystyki</p>
+            </div>
+            <div class="col-3 navigation_element">
+                <span class="material-icons">date_range</span>
+                <p>Plan Lekcji</p>
+            </div>
+        </div>
+    </div>
+</nav>
